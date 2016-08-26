67f53e7005b94954eb58dcbbff64ff430654e319
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-08-26 01:10:09 -0700

    asiopq::future

---
+++ include/asiopq/future.hpp
@@ -0,0 +1,31 @@
+/**
+ *	\file
+ */
+
+
+#include "configure.hpp"
+
+
+#ifdef ASIOPQ_USE_BOOST_FUTURE
+#ifndef BOOST_THREAD_PROVIDES_FUTURE
+#define BOOST_THREAD_PROVIDES_FUTURE
+#endif
+#include <boost/thread/future.hpp>
+#else
+#include <future>
+#endif
+
+
+namespace asiopq {
+
+
+	#ifdef ASIOPQ_USE_BOOST_FUTURE
+	using boost::future;
+	using boost::promise;
+	#else
+	using std::future;
+	using std::promise;
+	#endif
+
+
+}
